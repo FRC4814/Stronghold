275b4295be169f0423d25948e00a667878adc845
Mattster092 <mattster092@hotmail.com>
2016-02-22 19:49:13 -0500

    Use Java 8 Added project specific WPILib/NetworkTables

--- src/org/usfirst/frc/team4814/robot/MultiSpeedController.java
+++ src/org/usfirst/frc/team4814/robot/MultiSpeedController.java
@@ -42,13 +42,11 @@ public class MultiSpeedController implements SpeedController {
         }
     }
 
-	@Override
 	public void setInverted(boolean isInverted) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public boolean getInverted() {
 		// TODO Auto-generated method stub
 		return false;
